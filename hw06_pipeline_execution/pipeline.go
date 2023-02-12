da181d7812613c78a40001e70a303cd3a3b066c2
sealmo <sealmo@gmail.com>
2023-02-12 22:02:56 +0500

    HW6 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,40 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := wrapDone(in, done)
+
+	for _, stage := range stages {
+		out = wrapDone(stage(out), done)
+	}
+	return out
+}
+
+func wrapDone(in In, done In) Out {
+	out := make(Bi)
+
+	go func() {
+		defer func() {
+			close(out)
+			// Drain required if send data after done
+			// for range in {}
+		}()
+
+		for {
+			select {
+			case <-done:
+				return
+			default:
+			}
+			select {
+			case <-done:
+				return
+			case v, ok := <-in:
+				if !ok {
+					return
+				}
+				out <- v
+			}
+		}
+	}()
+	return out
 }
